ca2f0762ff2e31082e0d6f8b115e8b5244bc9b98
Justin Payne <thepaynotic@gmail.com>
2022-08-06 10:33:44 -0400

    second commit

--- src/gameOfWar/Card.java
+++ src/gameOfWar/Card.java
@@ -14,6 +14,7 @@ public class Card {
 	public String getSuit() {
 		return suit;
 	}
+	
 
 	public String getRank() {
 		return rank;
@@ -27,6 +28,18 @@ public class Card {
 	public String toString() {
 
 		return rank + " of " + suit;
+	}
+
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
 	}
 	
 
